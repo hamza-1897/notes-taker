085e0225a53f77398f16a7e128ed9506927bc09c
Muhammad Hamza <hanjaa1897@gmail.com>
2025-08-07 23:42:42 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+ import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500'>
+      note taking app using react.........
+    </div>
+  )
+}
+
+export default App
